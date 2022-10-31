ec2603b5dcc441397974993e3cd9b547df70aa65
dkoroni <82778132+dkoroni@users.noreply.github.com>
2022-10-31 16:17:49 -0700

    Add DataAccessLayer Class
    Create data access layer class

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/RegistrationDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+
+    /*    public enum ResultStatus
+        {
+            Unknown = 0,
+            Success = 1,
+            Faulty = 2
+        }*/
+    public class Result
+    {
+        public bool IsSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public object Payload { get; set; }
+
+    }
+    public class RegistrationDataAccess
+    {
+            public RegistrationDataAccess()
+            {
+
+            }
+            public RegistrationDataAccess(string tableName)
+            {
+
+            }
+            public Result RegisterAccount(string username, string passphrase)
+            {
+                var result = new Result();
+
+                return result;
+            }
+    }
+}
