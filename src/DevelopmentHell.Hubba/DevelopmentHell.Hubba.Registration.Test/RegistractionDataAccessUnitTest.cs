a41d762204f4efb6c56e933e8b6577f48457e48f
dkoroni <82778132+dkoroni@users.noreply.github.com>
2022-10-31 16:18:11 -0700

    Create RegistractionDataAccessUnitTest.cs
    Create initial test cases for Registration Data Access Layer unit tests

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Registration.Test/RegistractionDataAccessUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using DevelopmentHell.Hubba.SqlDataAccess;
+
+namespace DevelopmentHell.Hubba.Registration.Test
+{
+    [TestClass]
+    public class RegistrationSqlDataAccessUnitTest
+    {
+        [TestMethod]
+        public void ShouldCreateNewInstanceWithDefaultCtor()
+        {
+            // Arrange
+            var expected = typeof(RegistrationDataAccess);
+
+            // Act
+            var actual = new RegistrationDataAccess();
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.GetType() == expected);
+        }
+
+        public void ShouldCreateNewInstanceWithParameterCtor()
+        {
+            // Arrange
+            var expected = typeof(RegistrationDataAccess);
+            var expectedTableName = "Accounts";
+
+            // Act
+            var actual = new RegistrationDataAccess(expectedTableName);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.GetType() == expected);
+        }
+
+        public void ShouldRegisterNewAccountIntoDatabase()
+        {
+            // TODO: fill out test case
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        public void ShouldAccessExistingAccountInDatabase()
+        {
+            // TODO: fill out test case
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+
+}
