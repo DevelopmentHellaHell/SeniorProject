fa0f80e5414a7521b7cecb56ec9972d998c6d59a
dkoroni <82778132+dkoroni@users.noreply.github.com>
2022-11-01 00:41:40 -0700

    Add interfaces for Data Access Layers
    Add interfaces for each delete, inset, select, and update sql statements

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/IDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+    // I'm not sure if this interface is necessary, but it allows each data access member to have access to Result
+    // I'm not sure where else I'd put Result otherwise since I don't want to duplicate it across all interfaces or only have it in 1
+    internal interface IDataAccess
+    {
+
+    }
+    public class Result
+    {
+        public bool IsSuccessful { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public object? Payload { get; set; }
+
+    }
+}
---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/IDataAccessInsert.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+    internal interface IDataAccessInsert : IDataAccess
+    {
+        Result Insert(string databaseName, string tableName, List<Object> columnNames, List<Object> values);
+    }
+}
