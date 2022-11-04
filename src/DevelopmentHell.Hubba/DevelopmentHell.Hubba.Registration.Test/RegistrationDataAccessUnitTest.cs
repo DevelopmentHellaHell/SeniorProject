d7bee47552922f571ac32f3bcd500f1b0b2ed12d
dkoroni <82778132+dkoroni@users.noreply.github.com>
2022-11-03 20:02:12 -0700

    Abstracted UpdateDataAccess from RegistrationDataAccess
    Now have a separate update SQL statement class which is abstracted from the generic data access class.

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Registration.Test/RegistrationDataAccessUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using DevelopmentHell.Hubba.SqlDataAccess;
+
+namespace DevelopmentHell.Hubba.Registration.Test
+{
+    [TestClass]
+    public class RegistrationSqlDataAccessUnitTest
+    {
+        [TestMethod]
+        public void ShouldCreateNewInstanceWithParameterCtor()
+        {
+            // Arrange
+            var expected = typeof(InsertDataAccess);
+            var expectedDatabaseName = "DevelopmentHell.Hubba.Accounts";
+            var connectionString = String.Format(@"Server=localhost\SQLEXPRESS;Database={0};Integrated Security=True;Encrypt=False", expectedDatabaseName);
+
+            // Act
+            var actual = new UpdateDataAccess(connectionString);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.GetType() == expected);
+        }
+        [TestMethod]
+        public void ShouldRegisterNewAccountIntoDatabase()
+        {
+            // TODO: fill out test case
+            // Arrange
+            var expected = typeof(InsertDataAccess);
+            var expectedTableName = "Accounts";
+            var expectedDatabaseName = "DevelopmentHell.Hubba.Accounts";
+            var connectionString = String.Format(@"Server=localhost\SQLEXPRESS;Database={0};Integrated Security=True;Encrypt=False", expectedDatabaseName);
+            string email = @"Email@random.com";
+            string passphrase = "c0o1p4s5phra53";
+            Dictionary<string, object> newUserAccountCredentials = new Dictionary<string, object>();
+            newUserAccountCredentials.Add("email", email);
+            newUserAccountCredentials.Add("passphrase", passphrase);
+
+            // Act
+            var actual = new InsertDataAccess(connectionString);
+            DevelopmentHell.Hubba.SqlDataAccess.Result result = actual.Insert(expectedTableName, newUserAccountCredentials);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.GetType() == expected);
+        }
+
+        [TestMethod]
+        public void ShouldUpdateAccountInDatabase()
+        {
+            // TODO: fill out test case
+            // Arrange
+            var expected = typeof(UpdateDataAccess);
+            var expectedTableName = "Accounts";
+            var expectedDatabaseName = "DevelopmentHell.Hubba.Accounts";
+            var connectionString = String.Format(@"Server=localhost\SQLEXPRESS;Database={0};Integrated Security=True;Encrypt=False", expectedDatabaseName);
+            string email = @"Email@random.com";
+            string username = "secondbestusername";
+            Tuple<string, object> key = new Tuple<string, object>("email", email);
+            Dictionary<string, object> values = new()
+            {
+                { "username", username }
+            };
+
+            // Act
+            var actual = new UpdateDataAccess(connectionString);
+            DevelopmentHell.Hubba.SqlDataAccess.Result result = actual.Update(expectedTableName, key, values);
+            //int account_id = (int)(result.Payload);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.GetType() == expected);
+            //Assert.IsTrue((string)(actual.AccessEmail(account_id).Payload) == email);
+            //Assert.IsTrue((string)(actual.AccessPassphrase(account_id).Payload) == passphrase);
+        }
+        public void ShouldAccessExistingAccountInDatabase()
+        {
+            // TODO: fill out test case
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+
+}
---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/UpdateDataAccess.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+
+    /*    public enum ResultStatus
+        {
+            Unknown = 0,
+            Success = 1,
+            Faulty = 2
+        }*/
+
+    public class UpdateDataAccess
+    {
+        private string connectionPath;
+        public UpdateDataAccess(string inPath)
+        {
+            connectionPath = inPath;
+        }
+
+        private Result SendQuery(SqlCommand query)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionPath))
+                {
+                    query.Connection = conn;
+
+                    conn.Open();
+                    query.ExecuteNonQuery();
+                }
+                // TODO: figure out what to fill these with
+                return new Result();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                return new Result();
+            }
+        }
+
+        public Result Update(string table, Tuple<string, object> key, Dictionary<string, object> values)
+        {
+            using (SqlCommand insertQuery = new SqlCommand())
+            {
+
+                bool first = true;
+                StringBuilder sb = new();
+                foreach (var pair in values)
+                {
+                    if (!first)
+                    {
+                        sb.Append(", ");
+                    }
+                    first = false;
+                    sb.Append(pair.Key + " = @" + pair.Key);
+
+                    insertQuery.Parameters.Add(new SqlParameter(pair.Key, (string)(pair.Value)));
+                }
+                insertQuery.Parameters.Add(new SqlParameter(key.Item1, (string)(key.Item2)));
+                Console.WriteLine(String.Format("Update {0} SET {1} WHERE {2} = {3}", table, sb.ToString(), key.Item1, key.Item1));
+                insertQuery.CommandText = String.Format("Update {0} SET {1} WHERE {2} = {3}", table, sb.ToString(), key.Item1, key.Item1);
+
+                return SendQuery(insertQuery);
+            }
+        }
+
+    }
+}
