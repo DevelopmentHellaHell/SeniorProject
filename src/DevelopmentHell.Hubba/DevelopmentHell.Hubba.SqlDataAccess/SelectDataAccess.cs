396af8b42bf71c388481b2a0b8b7705a7d8c3921
dkoroni <82778132+dkoroni@users.noreply.github.com>
2022-11-04 16:45:46 -0700

    Create SelectDataAccess
    SelectDataAccess payload is a List<List<objects>> where the outer list is each row in the select statement, and the inner list is each column in the selected row.

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/SelectDataAccess.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+    public class SelectDataAccess
+    {
+        private string connectionPath; 
+        public SelectDataAccess(string inPath)
+        {
+            connectionPath = inPath;
+        }
+
+        private Result SendQuery(SqlCommand query, int columnLength)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionPath))
+                {
+                    query.Connection = conn;
+                    List<List<object>> payload = new();
+                    conn.Open();
+                    using(SqlDataReader reader = query.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            List<object> row = new();
+                            IDataRecord dataRecord = (IDataRecord)reader;
+                            for(int i = 0; i < columnLength; i++)
+                            {
+                                row.Add((dataRecord[i]));
+                            }
+                            payload.Add(row);
+                        }
+                        return new Result(true, "", payload);
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new Result(false, e.Message);
+            }
+        }
+
+        public Result Select(string table, List<string> columns, Dictionary<string, object> filters)
+        {
+            using (SqlCommand insertQuery = new SqlCommand())
+            {
+
+                bool first = true;
+                StringBuilder sbFilter = new();
+                StringBuilder sbColumn = new();
+                foreach (var pair in filters)
+                {
+                    if (!first)
+                    {
+                        sbFilter.Append(" AND ");
+                    }
+                    first = false;
+                    sbFilter.Append(pair.Key + " = @" + pair.Key);
+
+                    insertQuery.Parameters.Add(new SqlParameter(pair.Key, pair.Value));
+                }
+                first = true;
+                foreach(string column in columns)
+                {
+                    if (!first)
+                    {
+                        sbColumn.Append(", ");
+                    }
+                    first = false;
+                    sbColumn.Append(column);
+                }
+                insertQuery.CommandText = String.Format("SELECT {0} FROM {1} WHERE {2}", sbColumn.ToString(), table, sbFilter.ToString());
+
+                return SendQuery(insertQuery, columns.Count);
+            }
+        }
+
+    }
+}
