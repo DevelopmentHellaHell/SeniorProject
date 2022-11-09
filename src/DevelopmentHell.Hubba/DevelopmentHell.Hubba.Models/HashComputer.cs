fdf3036f05903bc8fd3c870b0469b2cf008760f7
ThuyTien Nguyen <xiu.tiennguyen@gmail.com>
2022-11-09 02:16:44 -0800

    HashComputer and Unit Test

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Models/HashComputer.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace DevelopmentHell.Hubba.Models
+{
+    public class HashComputer
+    {
+        private? string _plainText;
+        private byte[] _salt;
+        public string HashedText { get; set; }
+        public HashComputer() { }
+        public HashComputer(string text)
+        {
+            _plainText = text;
+        }
+
+        public string ComputeHash()
+        {
+            _salt = RandomNumberGenerator.GetBytes(128 / 8);
+            HashedText = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                                                password: _plainTest!,
+                                                salt: _salt,
+                                                prf: KeyDerivationPrf.HMACSHA256,
+                                                iterationCount: 100000,
+                                                numBytesRequested: 256 / 8));
+            return HashedText;
+        }
+    }
+}
---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Registration.Test/UnitTests/HashComputerUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using DevelopmentHell.Hubba.Models;
+
+
+namespace DevelopmentHell.Hubba.Registration.Test;
+
+[TestClass]
+public class HashComputerUnitTest
+{
+    [TestMethod]
+    public void ShouldCreateInstanceWithCtor()
+    {
+        // Arrange
+        var expected = typeof(Result);
+
+        // Act
+        var actual = new Result();
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.IsTrue(actual.GetType() == expected);
+    }
+
+    [TestMethod]
+    public void ShouldReturnCoorectIsSuccessfulValue()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+       
+    }
+
+    [TestMethod]
+    public void ShouldReturnAHash()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+}
