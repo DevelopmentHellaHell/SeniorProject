65196139ca7fa5c4fa2906f3a60f621cbbd99b96
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2022-11-08 20:34:56 -0800

    progress

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Logging.Test/Integration Tests/IntegrationTests.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Logging.Test/Integration Tests/IntegrationTests.cs
@@ -2,6 +2,7 @@ using DevelopmentHell.Hubba.Logging.Implementation;
 using DevelopmentHell.Hubba.SqlDataAccess;
 using DevelopmentHell.Hubba.SqlDataAccess.Implementation;
 using System.Configuration;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace DevelopmentHell.Hubba.Logging.Test
@@ -33,18 +34,34 @@ namespace DevelopmentHell.Hubba.Logging.Test
             var expectedDatabaseName = "DevelopmentHell.Hubba.Logs";
             var connectionString = String.Format(@"Server={0};Database={1};Integrated Security=True;Encrypt=False", ConfigurationManager.AppSettings["LoggingServer"], expectedDatabaseName);
             var dataAccess = new LoggingDataAccess(connectionString);
-            var sut = new Logger(dataAccess, Models.Category.VIEW);
 
-            // Act
-            var actual = await sut.Log(Models.LogLevel.INFO, "test", "test");
+			var category = Models.Category.VIEW;
+			var logLevel = Models.LogLevel.INFO;
+			var userName = "System";
+			var message = "test";
+			var sut = new Logger(dataAccess, category);
+			
+			var stopwatch = new Stopwatch();
+			
+			// Act
+			stopwatch.Start();
+			var actual = await sut.Log(logLevel, userName, message);
+			stopwatch.Stop();
 
             // Assert
             Assert.IsTrue(actual.IsSuccessful);
+			Assert.IsTrue(stopwatch.ElapsedMilliseconds <= 5000);
 
 			var dbChecker = new SelectDataAccess(connectionString);
-			var dbCheck = await dataAccess.SelectLogs(new List<string>() { "*" }, new Dictionary<string, object> { { "message", "test" } });
+			var dbCheck = await dataAccess.SelectLogs(new List<string>() { "id", "timestamp" }, new Dictionary<string, object> {
+				{ "category", category },
+				{ "logLevel", logLevel },
+				{ "userName", userName },
+				{ "message", message },
+			});
 
-			Assert.IsTrue(dbCheck.IsSuccessful);
+			Assert.IsTrue(dbCheck.Payload!.Count >= 1);
+
         }
 	}
 }
