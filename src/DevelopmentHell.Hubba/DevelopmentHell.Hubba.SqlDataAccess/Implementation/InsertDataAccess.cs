ddb7d6e84cd8fd9221c0047a806b28e195cb1e85
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2022-11-08 19:34:53 -0800

    Revert "app config"
    This reverts commit 804308f5925a30f9f06ce4232121ca6ff15606cf.

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/Implementation/InsertDataAccess.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/Implementation/InsertDataAccess.cs
@@ -1,7 +1,7 @@
 using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using DevelopmentHell.Hubba.Models;
-using System.Configuration;
+
 
 namespace DevelopmentHell.Hubba.SqlDataAccess.Implementation
 {
