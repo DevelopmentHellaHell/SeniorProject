f78f6caad54c4b8128fc64104d044e814d21e8f5
Dseus4 <tsumakig@gmail.com>
2022-11-08 17:25:53 -0800

    move dal changes upstream

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/Implementation/InsertDataAccess.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/Implementation/InsertDataAccess.cs
@@ -1,23 +1,19 @@
-﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
+using DevelopmentHell.Hubba.Models;
 
-namespace DevelopmentHell.Hubba.SqlDataAccess
+
+namespace DevelopmentHell.Hubba.SqlDataAccess.Implementation
 {
-    public class Result
-    {
-        public bool IsSuccessful { get; set; }
-        public string ErrorMessage { get; set; } = string.Empty;
-        public object? Payload { get; set; }
 
-    }
-    public class IDataAccess
+    public class InsertDataAccess
     {
         private string connectionPath;
-        public IDataAccess(string inPath)
+        public InsertDataAccess(string inPath)
         {
             connectionPath = inPath;
         }
-        private Result SendQuery(SqlCommand query)
+        private async Task<Result> SendQuery(SqlCommand query)
         {
             try
             {
@@ -25,26 +21,26 @@ namespace DevelopmentHell.Hubba.SqlDataAccess
                 {
                     query.Connection = conn;
 
-                    conn.Open();
-                    query.ExecuteNonQuery();
+                    await conn.OpenAsync().ConfigureAwait(false);
+                    await query.ExecuteNonQueryAsync().ConfigureAwait(false);
                 }
                 // TODO: figure out what to fill these with
                 return new Result();
             }
-            catch
+            catch (Exception e)
             {
-                return new Result();
+                return new Result(false, e.Message);
             }
         }
 
-        public Result Insert(string table, Dictionary<string, Object> values)
+        public async Task<Result> Insert(string table, Dictionary<string, object> values)
         {
             using (SqlCommand insertQuery = new SqlCommand())
             {
                 string columnString = "";
                 string valueString = "";
                 bool first = true;
-                foreach (KeyValuePair<string, Object> pair in values)
+                foreach (KeyValuePair<string, object> pair in values)
                 {
                     if (!first)
                     {
@@ -56,12 +52,11 @@ namespace DevelopmentHell.Hubba.SqlDataAccess
                     valueString += '@' + pair.Key;
 
 
-                    insertQuery.Parameters.Add(new SqlParameter(pair.Key, (string)(pair.Value)));
+                    insertQuery.Parameters.Add(new SqlParameter(pair.Key, pair.Value));
                 }
+                insertQuery.CommandText = string.Format("INSERT into {0}({1}) VALUES ({2})", table, columnString, valueString);
 
-                insertQuery.CommandText = String.Format("INSERT into {0}({1}) VALUES {2}", table, columnString, valueString);
-
-                return SendQuery(insertQuery);
+                return await SendQuery(insertQuery).ConfigureAwait(false);
             }
         }
     }
