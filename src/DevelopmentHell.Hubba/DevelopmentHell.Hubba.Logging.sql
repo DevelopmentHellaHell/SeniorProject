a908cc5e8ada7e8d25969a3619bbdfbf1d2084e4
Dseus4 <tsumakig@gmail.com>
2022-11-08 17:00:47 -0800

    export schema logging scripts

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Logging.sql
@@ -0,0 +1,106 @@
+﻿USE [master]
+GO
+/****** Object:  Database [DevelopmentHell.Hubba.Logs]    Script Date: 11/8/2022 4:50:51 PM ******/
+CREATE DATABASE [DevelopmentHell.Hubba.Logs]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DevelopmentHell.Hubba.Logs', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Logs.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DevelopmentHell.Hubba.Logs_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Logs_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DevelopmentHell.Hubba.Logs].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET RECOVERY FULL 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET  MULTI_USER 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'DevelopmentHell.Hubba.Logs', N'ON'
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET QUERY_STORE = OFF
+GO
+USE [DevelopmentHell.Hubba.Logs]
+GO
+/****** Object:  Table [dbo].[Logs]    Script Date: 11/8/2022 4:50:51 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Logs](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[timestamp] [datetime] NOT NULL,
+	[logLevel] [int] NOT NULL,
+	[category] [int] NOT NULL,
+	[userName] [nvarchar](50) NOT NULL,
+	[message] [nvarchar](200) NOT NULL,
+ CONSTRAINT [PK_Logs] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Logs] SET  READ_WRITE 
+GO
