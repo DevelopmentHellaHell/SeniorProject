76b95d927399b0841eed7eda7856e2c18b24be5a
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2022-12-04 20:01:32 -0800

    fix

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/AuthenticationDataAccess.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/AuthenticationDataAccess.cs
@@ -15,25 +15,40 @@ namespace DevelopmentHell.Hubba.SqlDataAccess
 			_updateDataAccess = new UpdateDataAccess(connectionString);
 		}
 
-		public async Task<Result> SelectAccount(string email, string password)
+		public async Task<Result> SelectUserAccount(string email, string password)
 		{
-			Result selectResult = await _selectDataAccess.Select("UserAccount", new List<string> { "Id" }, new Dictionary<string, object>
+			Result selectResult = await _selectDataAccess.Select("UserAccount", new List<string> { "Id" }, new()
 			{
-				{ "Email", email },
-				{ "Password", password },
+				new("Email", "=", email),
+				new("Password", "=", password),
 			}).ConfigureAwait(false);
 			return selectResult;
 		}
 
-		public async Task<Result> CreateOTP(string email, string passphrase)
+		public async Task<Result> CreateUserOTP(string email, string passphrase)
 		{
 			DateTime expirationDateTime = DateTime.UtcNow.AddSeconds(Convert.ToDouble(ConfigurationManager.AppSettings["OTPExpirationOffsetSeconds"]!));
 			Result updateResult = await _updateDataAccess.Update("UserOTP", new Tuple<string, object>("Email", email), new Dictionary<string, object>
 			{
 				{ "ExpirationDateTime", expirationDateTime },
 				{ "Passphrase", passphrase },
-			});
+			}).ConfigureAwait(false);
 			return updateResult;
+		}
+
+		public async Task<Result> SelectUserOTP(string otp, string email)
+		{
+			DateTime now = DateTime.UtcNow;
+			Result selectResult = await _selectDataAccess.Select(
+				TableManip.InnerJoinTables("UserOTP", "UserAccount", "Email", "Email"),
+				new() { "*" },
+				new()
+				{
+					new("passphrase", "=", otp),
+					new("UserOTP.email", "=", email),
+					new(now, "<", "UserOTP.ExpirationDateTime")
+				});
+			return selectResult;
 		}
 	}
 }
