d707d4f75ab18761f8c92cb4619078ce1efeb57d
Dseus4 <tsumakig@gmail.com>
2022-12-09 18:51:02 -0800

    update encrypt

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Encryption/EncryptionService.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Encryption/EncryptionService.cs
@@ -1,59 +1,61 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace DevelopmentHell.Hubba.Cryptography.Service
 {
 	public class EncryptionService
 	{
-		public static byte[] Encrypt(string plainText, byte[] key)
-		{
-			byte[] encrypted;
-			using (AesManaged aes = new AesManaged())
-			{
-				using (ICryptoTransform encryptor = aes.CreateEncryptor(key, aes.IV))
-				{
-					using (MemoryStream ms = new MemoryStream())
-					{
-						// Create crypto stream using the CryptoStream class. This class is the key to encryption
-						// and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream
-						// to encrypt
-						using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-						{
-							// Create StreamWriter and write data to a stream
-							using (StreamWriter sw = new StreamWriter(cs))
-								sw.Write(plainText);
-							encrypted = ms.ToArray();
-						}
-					}
-				}
-			}
+        //TODO: move to config file
+        private static readonly byte[] _aesKey = Encoding.ASCII.GetBytes("gVkYp2s5v8y/B?E(H+MbQeThWmZq4t6w");
+        //private static readonly byte[] _aesIV = Encoding.ASCII.GetBytes("")
 
-			return encrypted;
-		}
+        private static Aes _alg = Aes.Create();
 
-		//public static string Decrypt(byte[] encryptedText, byte[] key)
-		//{
-		//	string decrypted;
-		//	using (AesManaged aes = new AesManaged())
-		//	{
-		//		using (ICryptoTransform encryptor = aes.CreateDecryptor(key, aes.IV))
-		//		{
-		//			using (MemoryStream ms = new MemoryStream())
-		//			{
-		//				// Create crypto stream using the CryptoStream class. This class is the key to encryption
-		//				// and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream
-		//				// to encrypt
-		//				using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-		//				{
-		//					// Create StreamWriter and write data to a stream
-		//					using (StreamWriter sw = new StreamWriter(cs))
-		//						sw.Write(encryptedText);
-		//					//decrypted = ms.; // TODO
-		//				}
-		//			}
-		//		}
-		//	}
+        public static byte[] Encrypt(string plainText)
+        {
 
-		//	return decrypted;
-		//}
-	}
+            byte[] encrypted;
+
+            _alg.Key = _aesKey;
+            ICryptoTransform encryptor = _alg.CreateEncryptor();
+
+            // Create MemoryStream
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Create crypto stream using the CryptoStream class.
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    // Create StreamWriter and write data to a stream
+                    using (StreamWriter sw = new StreamWriter(cs))
+                        sw.Write(plainText);
+                    encrypted = ms.ToArray();
+                }
+            }
+
+            // Return encrypted data
+            return encrypted;
+        }
+        public static string Decrypt(byte[] encrypted)
+        {
+            string output;
+
+            _alg.Key = _aesKey;
+            ICryptoTransform decryptor = _alg.CreateDecryptor();
+
+            // Create MemoryStream
+            using (MemoryStream ms = new MemoryStream(encrypted))
+            {
+                // Create crypto stream using the CryptoStream class.
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    // Create StreamWriter and write data to a stream
+                    using (StreamReader sr = new StreamReader(cs))
+                        output = sr.ReadToEnd();
+                }
+            }
+
+            // Return encrypted data
+            return output;
+        }
+    }
 }
