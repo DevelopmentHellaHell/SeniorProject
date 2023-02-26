19341c475443afa02e569ff689d85971fd727011
ThuyTien Nguyen <xiu.tiennguyen@gmail.com>
2023-02-25 19:38:30 -0800

    Update App.tsx

--- src/DevelopmentHell.Hubba.Client/src/App.tsx
+++ src/DevelopmentHell.Hubba.Client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Account from "./pages/Account/Account";
 import Analytics from "./pages/Analytics/Analytics";
-import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Logout from "./pages/Logout/Logout";
-import Registration from "./pages/Registration/Registration";
+import Notification from "./pages/Notification/Notification";
 import Otp from "./pages/Otp/Otp";
+import Registration from "./pages/Registration/Registration";
 
 import "./Theme.css";
 
@@ -19,13 +20,17 @@ const App: React.FC<Props> = (props) => {
 		<div className="App">
 			<BrowserRouter>
 				<Routes>
+					//add new page in alphabetical order
 					<Route path="/" element={<Home />} />
-					<Route path="/registration" element={<Registration />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/otp" element={<Otp />} />
-					<Route path="/logout" element={<Logout />} />
-					<Route path="/analytics" element={<Analytics />} />
+
 					<Route path="/account" element={<Account />} />
+					<Route path="/analytics" element={<Analytics />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/logout" element={<Logout />} />
+					<Route path="/notification" element={<Notification />} />
+					<Route path="/otp" element={<Otp />} />
+					<Route path="/registration" element={<Registration />} />
+					
 				</Routes>
 			</BrowserRouter>
 		</div>
