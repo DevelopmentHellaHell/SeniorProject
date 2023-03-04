917a9c75da3e39738f592837913fe172174c7cd6
dkoroni <82778132+dkoroni@users.noreply.github.com>
2023-03-04 03:22:00 -0800

    Account Deletion Controller

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.WebAPI/Controllers/AccountDeletionController.cs
@@ -0,0 +1,44 @@
+﻿using DevelopmentHell.Hubba.AccountDeletion.Manager.Abstraction;
+using DevelopmentHell.Hubba.WebAPI.DTO.AccountDeletion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopmentHell.Hubba.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountDeletionController : Controller
+    {
+        private readonly IAccountDeletionManager _accountDeletionManager;
+
+        public AccountDeletionController(IAccountDeletionManager accountDeletionManager)
+        {
+            _accountDeletionManager = accountDeletionManager;
+        }
+
+#if DEBUG
+        [HttpGet]
+        [Route("health")]
+        public Task<IActionResult> HeathCheck()
+        {
+            return Task.FromResult<IActionResult>(Ok("Healthy"));
+        }
+#endif
+
+        [HttpPost]
+        [Route("deleteaccount")]
+        public async Task<IActionResult> DeleteAccount(AccountDeletionDTO accountDeletionDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid request.");
+            }
+
+            var result = await _accountDeletionManager.DeleteAccount(accountDeletionDTO.AccountId);
+            if (!result.IsSuccessful)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+            return Ok();
+        }
+    }
+}
