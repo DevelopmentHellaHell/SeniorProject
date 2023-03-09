a587b3cbb3d5f1a3abd84792e439b2f603d857cf
ThuyTien Nguyen <xiu.tiennguyen@gmail.com>
2023-03-08 21:09:21 -0800

    where did my code go?

--- src/DevelopmentHell.Hubba.Client/cypress.config.ts
+++ src/DevelopmentHell.Hubba.Client/cypress.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env:{
+    registrationUrl: "http://localhost:3000/registration",
+    loginUrl: "http://localhost:3000/login"
+  }
 });
