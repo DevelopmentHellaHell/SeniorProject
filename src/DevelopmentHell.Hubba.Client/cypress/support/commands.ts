ac17773469a6f5ba71a75022f4eb4b4082e7c198
ThuyTien Nguyen <xiu.tiennguyen@gmail.com>
2023-03-11 20:47:16 -0800

    More tests for login

--- src/DevelopmentHell.Hubba.Client/cypress/support/commands.ts
+++ src/DevelopmentHell.Hubba.Client/cypress/support/commands.ts
@@ -25,28 +25,35 @@ Cypress.Commands.add('registerViaApi', (email: string, password: string) => {
  */
 Cypress.Commands.add('loginViaUI', (email: string, password: string) => {
     cy.session([email, password], () => {
-        cy.visit('/login');
-        cy.get('#email').type(Cypress.env('realEmail')).should('have.value', Cypress.env('realEmail'));
-        cy.get('#password').type(Cypress.env('standardPassword')).should('have.value', Cypress.env('standardPassword'));
+        cy.visit("/login");
+        let startTimer: number;
+        cy.get('#email').as('email').type(Cypress.env('realEmail')).should('have.value', Cypress.env('realEmail'));
+        cy.get('#password').as('password').type(Cypress.env('standardPassword')).should('have.value', Cypress.env('standardPassword'));
         cy.contains('Submit').click()
             .then(() => {
-                cy.get('.otp-card')
-                    .should('exist').and('be.visible');
-                cy.request('GET', Cypress.env('serverUrl') + "/tests/getotp")
-                    .then((response) => {
-                        cy.wrap(response.body).as('returnedOtp');
-                    });
+                //valid email, password, show OTP card
+                cy.get('.otp-card').should('exist').and('be.visible');
+            })
 
+        //get OTP from the database
+        cy.request('GET', Cypress.env('serverUrl') + "/tests/getotp")
+            .then((response) => {
+                cy.wrap(response.body).as('returnedOtp');
                 cy.get('@returnedOtp')
-                    .then((otp) => {
-                        cy.get('#otp')
-                            .type(otp)
-                            .should('have.value', otp);
-                    });
-                cy.contains('Submit').click()
-                    .then(() => {
-                        cy.url().should('eq', Cypress.env('baseUrl') + '/');
-                        cy.get('.nav-user').should('exist').and('be.visible')
+                    .then((otpString) => {
+                        let otp = otpString.toString();
+                        cy.get('#otp').type(otp).should('have.value', otp);
+                        startTimer = Date.now();
+                        // time the login operation            
+                        cy.contains('Submit').click()
+                            .then(() => {
+                                let endTimer: number = Date.now() - startTimer;
+                                expect(endTimer).lessThan(5000);
+                            });
+                        cy.url().should('eq', Cypress.env('baseUrl')+'/');
+                        cy.get('.nav-user').should('exist').and('be.visible');
+                        cy.contains('Sign Up').should('not.exist');
+                        cy.contains('Login').should('not.exist');
                     });
             });
     })
