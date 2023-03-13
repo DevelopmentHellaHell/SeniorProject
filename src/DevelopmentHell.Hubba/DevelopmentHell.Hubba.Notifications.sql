7c3e004940429167113f6fe4001204dead1ab900
KevinDinh01 <kevin.dinh@student.csulb.edu>
2023-03-13 02:53:17 -0700

    Update DevelopmentHell.Hubba.Notifications.sql

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Notifications.sql
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Notifications.sql
@@ -7,13 +7,13 @@ GO
 USE [master]
 GO
 
-/****** Object:  Database [DevelopmentHell.Hubba.Notifications]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Database [DevelopmentHell.Hubba.Notifications]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE DATABASE [DevelopmentHell.Hubba.Notifications]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'DevelopmentHell.Hubba.Notifications', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Notifications.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+( NAME = N'DevelopmentHell.Hubba.Notifications', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Notifications.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'DevelopmentHell.Hubba.Notifications_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Notifications_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+( NAME = N'DevelopmentHell.Hubba.Notifications_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\DevelopmentHell.Hubba.Notifications_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
 ALTER DATABASE [DevelopmentHell.Hubba.Notifications] SET COMPATIBILITY_LEVEL = 150
@@ -89,57 +89,57 @@ ALTER DATABASE [DevelopmentHell.Hubba.Notifications] SET QUERY_STORE = ON
 GO
 ALTER DATABASE [DevelopmentHell.Hubba.Notifications] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
-/****** Object:  Login [ZOJIRUSHI\Kevin]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [ZOJIRUSHI\Kevin]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [ZOJIRUSHI\Kevin] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT SERVICE\Winmgmt]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT SERVICE\Winmgmt]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT SERVICE\Winmgmt] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT SERVICE\SQLWriter]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT SERVICE\SQLWriter]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT SERVICE\SQLWriter] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT SERVICE\SQLTELEMETRY]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT SERVICE\SQLTELEMETRY]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT SERVICE\SQLTELEMETRY] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT SERVICE\SQLSERVERAGENT]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT SERVICE\SQLSERVERAGENT]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT SERVICE\SQLSERVERAGENT] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT Service\MSSQLSERVER]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT Service\MSSQLSERVER]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT Service\MSSQLSERVER] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
-/****** Object:  Login [NT AUTHORITY\SYSTEM]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [NT AUTHORITY\SYSTEM]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [NT AUTHORITY\SYSTEM] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.User]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.User]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [DevelopmentHell.Hubba.SqlUser.User] WITH PASSWORD=N'password', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
 GO
 ALTER LOGIN [DevelopmentHell.Hubba.SqlUser.User] ENABLE
 GO
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.Notification]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.Notification]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [DevelopmentHell.Hubba.SqlUser.Notification] WITH PASSWORD=N'password', DEFAULT_DATABASE=[DevelopmentHell.Hubba.Notifications], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 ALTER LOGIN [DevelopmentHell.Hubba.SqlUser.Notification] ENABLE
 GO
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.Logging]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [DevelopmentHell.Hubba.SqlUser.Logging]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [DevelopmentHell.Hubba.SqlUser.Logging] WITH PASSWORD=N'password', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 ALTER LOGIN [DevelopmentHell.Hubba.SqlUser.Logging] ENABLE
 GO
-/****** Object:  Login [BUILTIN\Users]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Login [BUILTIN\Users]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE LOGIN [BUILTIN\Users] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english]
 GO
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [##MS_PolicyTsqlExecutionLogin##]    Script Date: 3/10/2023 12:30:08 AM ******/
-CREATE LOGIN [##MS_PolicyTsqlExecutionLogin##] WITH PASSWORD=N'VF5EXEMg0+u/qRDOhef1eSBYlUat4bVG2qhYFskIAdw=', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+/****** Object:  Login [##MS_PolicyTsqlExecutionLogin##]    Script Date: 3/13/2023 2:49:46 AM ******/
+CREATE LOGIN [##MS_PolicyTsqlExecutionLogin##] WITH PASSWORD=N'cyWmVtEa3fzqFeEro11Spbp7xzdAX9Kt9+xNn5EBddY=', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 ALTER LOGIN [##MS_PolicyTsqlExecutionLogin##] DISABLE
 GO
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [##MS_PolicyEventProcessingLogin##]    Script Date: 3/10/2023 12:30:08 AM ******/
-CREATE LOGIN [##MS_PolicyEventProcessingLogin##] WITH PASSWORD=N'LL6GFoxTSQtX1s5Nc/8ff9jwM2xWCKTZv6VzxWZjwH4=', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+/****** Object:  Login [##MS_PolicyEventProcessingLogin##]    Script Date: 3/13/2023 2:49:46 AM ******/
+CREATE LOGIN [##MS_PolicyEventProcessingLogin##] WITH PASSWORD=N'Ti0mD77v+YChggKNXd1dyayli8xqqmfEJFP9GGowxiE=', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 ALTER LOGIN [##MS_PolicyEventProcessingLogin##] DISABLE
 GO
@@ -157,15 +157,8 @@ ALTER SERVER ROLE [sysadmin] ADD MEMBER [NT Service\MSSQLSERVER]
 GO
 USE [DevelopmentHell.Hubba.Notifications]
 GO
-/****** Object:  User [DevelopmentHell.Hubba.SqlUser.Notifications]    Script Date: 3/10/2023 12:30:08 AM ******/
-CREATE USER [DevelopmentHell.Hubba.SqlUser.Notifications] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[dbo]
-GO
-/****** Object:  User [DevelopmentHell.Hubba.SqlUser.Notification]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  User [DevelopmentHell.Hubba.SqlUser.Notification]    Script Date: 3/13/2023 2:49:46 AM ******/
 CREATE USER [DevelopmentHell.Hubba.SqlUser.Notification] FOR LOGIN [DevelopmentHell.Hubba.SqlUser.Notification] WITH DEFAULT_SCHEMA=[dbo]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [DevelopmentHell.Hubba.SqlUser.Notifications]
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [DevelopmentHell.Hubba.SqlUser.Notifications]
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [DevelopmentHell.Hubba.SqlUser.Notification]
 GO
@@ -173,16 +166,59 @@ ALTER ROLE [db_datawriter] ADD MEMBER [DevelopmentHell.Hubba.SqlUser.Notificatio
 GO
 GRANT CONNECT TO [DevelopmentHell.Hubba.SqlUser.Notification] AS [dbo]
 GO
-GRANT CONNECT TO [DevelopmentHell.Hubba.SqlUser.Notifications] AS [dbo]
-GO
 GRANT VIEW ANY COLUMN ENCRYPTION KEY DEFINITION TO [public] AS [dbo]
 GO
 GRANT VIEW ANY COLUMN MASTER KEY DEFINITION TO [public] AS [dbo]
 GO
-/****** Object:  Table [dbo].[NotificationSettings]    Script Date: 3/10/2023 12:30:08 AM ******/
+/****** Object:  Table [dbo].[NotificationSettings]    Script Date: 3/13/2023 2:49:46 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NotificationSettings](
+	[UserId] [int] NOT NULL,
+	[SiteNotifications] [bit] NOT NULL,
+	[EmailNotifications] [bit] NOT NULL,
+	[TextNotifications] [bit] NOT NULL,
+	[TypeScheduling] [bit] NOT NULL,
+	[TypeWorkspace] [bit] NOT NULL,
+	[TypeProjectShowcase] [bit] NOT NULL,
+	[TypeOther] [bit] NOT NULL,
+ CONSTRAINT [PK_NotificationSettings] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER AUTHORIZATION ON [dbo].[NotificationSettings] TO  SCHEMA OWNER 
+GO
+/****** Object:  Table [dbo].[UserNotifications]    Script Date: 3/13/2023 2:49:47 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserNotifications](
+	[UserId] [int] NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+	[Tag] [int] NOT NULL,
+	[Message] [varchar](256) NOT NULL,
+	[Hide] [bit] NOT NULL,
+	[NotificationId] [int] IDENTITY(1,1) NOT NULL,
+ CONSTRAINT [PK_UserNotifications] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[DateCreated] ASC,
+	[NotificationId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER AUTHORIZATION ON [dbo].[UserNotifications] TO  SCHEMA OWNER 
+GO
+USE [master]
+GO
+ALTER DATABASE [DevelopmentHell.Hubba.Notifications] SET  READ_WRITE 
+GO
+
 GO
 CREATE TABLE [dbo].[NotificationSettings](
 	[UserId] [int] NOT NULL,
