1bc81d04006495d26fd6b1f3069a1e3c0929234e
KevinDinh01 <kevin.dinh@student.csulb.edu>
2023-04-20 02:25:46 -0700

    test

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/Abstractions/IAccountSystem.cs
@@ -0,0 +1,9 @@
+﻿using DevelopmentHell.Hubba.Models
+
+namespace DevelopmentHell.Hubba.SqlDataAccess.Abstractions
+{
+    public interface IAccountSystem
+    {
+        Task<Result<List<Dictionary<string, string>>>> GetPasswordData(string email);
+    }
+}
---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.SqlDataAccess/AccountSystemDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using DevelopmentHell.Hubba.SqlDataAccess.Implementations;
+
+namespace DevelopmentHell.Hubba.SqlDataAccess
+{
+    public class AccountSystemDataAccess
+    {
+        private InsertDataAccess _insertDataAccess;
+        private UpdateDataAccess _updateDataAccess;
+        private SelectDataAccess _selectDataAccess;
+        private DeleteDataAccess _deleteDataAccess;
+        private string _tableName;
+
+        public AccountSystemDataAccess(string connectionString, string tableName)
+        {
+            _insertDataAccess = new InsertDataAccess(connectionString);
+            _updateDataAccess = new UpdateDataAccess(connectionString);
+            _selectDataAccess = new SelectDataAccess(connectionString);
+            _deleteDataAccess = new DeleteDataAccess(connectionString);
+            _tableName = tableName;
+        }
+    }
+}
