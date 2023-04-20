dad7f070e7bc9d0c1dc3a1dee5bb366f2408f7a2
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2023-04-20 04:21:22 -0700

    search categories method

--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Discovery.Manager/Abstractions/IDiscoveryManager.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Discovery.Manager/Abstractions/IDiscoveryManager.cs
@@ -5,5 +5,6 @@ namespace DevelopmentHell.Hubba.Discovery.Manager.Abstractions
 	public interface IDiscoveryManager
 	{
 		Task<Result<Dictionary<string, List<Dictionary<string, object>>?>>> GetCurated(int offset);
+		Task<Result<List<Dictionary<string, object>>>> GetSearch(string query, string category, string filter, int offset);
 	}
 }
--- src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Discovery.Service/Abstractions/IDiscoveryService.cs
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.Discovery.Service/Abstractions/IDiscoveryService.cs
@@ -5,5 +5,6 @@ namespace DevelopmentHell.Hubba.Discovery.Service.Abstractions
 	public interface IDiscoveryService
 	{
 		Task<Result<Dictionary<string, List<Dictionary<string, object>>?>>> GetCurated(int offset);
+		Task<Result<List<Dictionary<string, object>>>> GetSearch(string query, string category, string filter, int offset);
 	}
 }
