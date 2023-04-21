3e9165e8f83b7507023f258e9192c3be1d276f2c
Jett Sonoda <49688189+ItsJustJett@users.noreply.github.com>
2023-04-21 07:55:57 -0700

    Controller start

---
+++ src/DevelopmentHell.Hubba/DevelopmentHell.Hubba.WebAPI/Controllers/ListingProfileController.cs
@@ -0,0 +1,60 @@
+﻿using DevelopmentHell.Hubba.WebAPI.DTO.Discovery;
+using DevelopmentHell.ListingProfile.Manager.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopmentHell.Hubba.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListingProfileController : HubbaController
+    {
+        private readonly IListingProfileManager _listingProfileManager;
+
+        public ListingProfileController(IListingProfileManager listingProfileManager)
+        {
+            _listingProfileManager = listingProfileManager;
+        }
+
+        [HttpGet]
+        [Route("createListing")]
+        public async Task<IActionResult> CreateListing(string title)
+        {
+            return await GuardedWorkload(async () =>
+            {
+                if (!ModelState.IsValid)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+
+                var result = await _listingProfileManager.CreateListing(title).ConfigureAwait(false);
+                if (!result.IsSuccessful)
+                {
+                    return StatusCode(result.StatusCode, result.ErrorMessage);
+                }
+
+                return StatusCode(result.StatusCode);
+            }).ConfigureAwait(false);
+        }
+
+        [HttpGet]
+        [Route("viewListing")]
+        public async Task<IActionResult> GetSearch(int listingId)
+        {
+            return await GuardedWorkload(async () =>
+            {
+                if (!ModelState.IsValid)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+
+                var result = await _listingProfileManager.ViewListing(listingId).ConfigureAwait(false);
+                if (!result.IsSuccessful)
+                {
+                    return StatusCode(result.StatusCode, result.ErrorMessage);
+                }
+
+                return StatusCode(result.StatusCode, result.Payload);
+            }).ConfigureAwait(false);
+        }
+    }
+}
