e47c5c0506cf2578daaf09ecae2dd5e58c3671ef
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2023-04-21 21:10:18 -0700

    fix thumbnails

--- src/DevelopmentHell.Hubba.Client/src/pages/DiscoverPage/CollaboratorCard/CollaboratorCard.tsx
+++ src/DevelopmentHell.Hubba.Client/src/pages/DiscoverPage/CollaboratorCard/CollaboratorCard.tsx
@@ -10,7 +10,7 @@ interface ICollaboratorCardProps {
 const CollaboratorCard: React.FC<ICollaboratorCardProps> = (props) => {
     return (
         <div className="collaborator-card" onClick={() => { alert(props.data.CollaboratorId) }}>
-            <img className="thumbnail" src="https://www.elitetreecare.com/wp-content/uploads/2019/01/oak-tree.jpg" alt="alternatetext" />
+            <img className="thumbnail" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="alternatetext" />
             <div className="info-block">
                 <p className="title">{props.data.Name + "dklajsdlkajslkaaaaaaaaaad"}</p>
                 <div className="rating-block">
