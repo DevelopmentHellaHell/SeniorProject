509acf2fc10541f43e504507de4d8efb3b2dfd07
Bryan Tran <Bryan.Tran02@student.csulb.edu>
2023-04-22 01:12:43 -0700

    remove

--- src/DevelopmentHell.Hubba.Client/src/pages/DiscoverPage/CollaboratorCard/CollaboratorCard.tsx
+++ src/DevelopmentHell.Hubba.Client/src/pages/DiscoverPage/CollaboratorCard/CollaboratorCard.tsx
@@ -12,7 +12,7 @@ const CollaboratorCard: React.FC<ICollaboratorCardProps> = (props) => {
         <div className="collaborator-card" onClick={() => { alert(props.data.CollaboratorId) }}>
             <img className="thumbnail" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="alternatetext" />
             <div className="info-block">
-                <p className="title">{props.data.Name + "dklajsdlkajslkaaaaaaaaaad"}</p>
+                <p className="title">{props.data.Name}</p>
                 <div className="rating-block">
                     <span className="star">★</span><p className="count">{props.data.TotalVotes}</p>
                 </div>
