d13846560f0e3500cfe5c09bdb702ed72768313b
Jett Sonoda <49688189+ItsJustJett@users.noreply.github.com>
2023-04-24 11:48:52 -0700

    e2e test

--- src/DevelopmentHell.Hubba.Client/src/pages/ListingPage/ViewListingPage.tsx/ViewListingRatingsPage/ListingRatingCard/ListingRatingCard.tsx
+++ src/DevelopmentHell.Hubba.Client/src/pages/ListingPage/ViewListingPage.tsx/ViewListingRatingsPage/ListingRatingCard/ListingRatingCard.tsx
@@ -5,13 +5,14 @@ interface IListingRatingCardProps {
 }
 
 const ListingRatingCard: React.FC<IListingRatingCardProps> = (props) => {
+    console.log(props.rating);
     return (
         <div>
             
             <tr>
                 <td>{props.rating.rating}</td>
-                <td>{props.rating.anonymous ? "Anonymous" : props.rating.username}</td>
                 <td>{props.rating.comment}</td>
+                <td>{props.rating.anonymous ? "Anonymous" : props.rating.username}</td>
                 <td>{props.rating.lastEdited ? new Date(props.rating.lastEdited).toLocaleString() : '-'}</td>
                 </tr>
 
